f8063f9e2d1729b0a53a17e85290a8d112dbc1b4
Ualnomis <ualnamnuhc@gmail.com>
2025-10-03 15:34:42 -0400

    feat: change wiki math svg to png

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+
+const mathImages: NodeListOf<HTMLImageElement> = document.querySelectorAll(".mwe-math-element img");
+
+for (const mathImage of mathImages) {
+  mathImage.src = mathImage.src.replace("svg", "png");
+}
